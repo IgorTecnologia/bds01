76558773eb26ee417687cf32012786e736cb3468
IgorTecnologia <iguigu2019@gmail.com>
2024-04-11 20:38:01 -0300

    Tests passing

---
+++ src/main/java/com/devsuperior/bds01/services/DepartmentService.java
@@ -0,0 +1,25 @@
+package com.devsuperior.bds01.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.bds01.dto.DepartmentDTO;
+import com.devsuperior.bds01.entities.Department;
+import com.devsuperior.bds01.repositories.DepartmentRepository;
+
+@Service
+public class DepartmentService {
+
+	@Autowired
+	DepartmentRepository repository;
+	
+	public List<DepartmentDTO> findAll(){
+		List<Department> entity = repository.findAll(Sort.by("name"));
+		
+		 return entity.stream().map(x -> new DepartmentDTO(x)).collect(Collectors.toList());
+	}
+}
